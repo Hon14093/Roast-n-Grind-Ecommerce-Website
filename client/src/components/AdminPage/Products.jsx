1abf70018bba41839c4ef1abd71bf2f5936adab9
halap <halaphin6@gmai.com>
2025-02-27 17:11:17 +0700

    ok

--- client/src/components/AdminPage/Products.jsx
+++ client/src/components/AdminPage/Products.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { Toaster } from "@/components/ui/sonner"
 import {
     Breadcrumb,
     BreadcrumbItem,
--- client/src/hooks/productAPI.jsx
+++ client/src/hooks/productAPI.jsx
@@ -1,16 +1,6 @@
 import axios from 'axios';
 
-export const getAllProduct = async () => {
-    const getData = async (e) => {
-        axios.get('http://localhost:5000/api/products/details')
-        .then(result => {
-            setData(result.data.products);
-            console.log(result.data.products);
-        })
-        .catch(result => console.log(result))
-    }
-    
-}
+
 
 export const getProductData = async (setData) => {
     try {
