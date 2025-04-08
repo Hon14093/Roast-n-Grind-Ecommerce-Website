9a4c932d9dde25126a083fc313c5d10b5bd7b775
halap <halaphin6@gmai.com>
2025-04-08 20:52:54 +0700

    no changes

--- client/src/components/AuthPages/LoginBody.jsx
+++ client/src/components/AuthPages/LoginBody.jsx
@@ -1,13 +1,12 @@
-// LoginBody.jsx
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../../context/AuthContext";
-import axios from "axios";
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useAuth } from '../../context/AuthContext';
+import axios from 'axios'
 
 function LoginBody() {
     const { user, login, logout} = useAuth();
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
     const navigate = useNavigate();
 
     const handleSubmitLogin = async (e) => {
@@ -26,71 +25,66 @@ function LoginBody() {
             if (result.data.is_admin) {
                 navigate('/admin');
             } else {
-                setError("Không nhận được token từ server.");
+                navigate('/');
             }
         })
-        .catch(result => console.log(result))
-
+        .catch(result => {
+            alert('Đăng nhập không thành công!');
+            console.log(result);
+        })
     }
 
     return (
-        <section className="bg-blue-100 w-full flex h-screen">
-            <article className="w-1/2">
-                <div id="loginBG"></div>
+        <section className='bg-blue-100 w-full flex h-screen'>
+            <article className='w-1/2'>
+                <div id='loginBG'></div>
             </article>
 
-            <article className="w-1/2 bg-primaryGreen text-bgColor items-center justify-center">
-                <div className="h-screen w-full flex flex-col justify-center md:px-[40px] sm:px-[20px]">
-                    <div className="mx-auto w-full lg:max-w-[400px]">
+            <article className='w-1/2 bg-primaryGreen text-bgColor items-center justify-center'>
+                <div className='h-screen w-full flex flex-col justify-center md:px-[40px] sm:px-[20px]'>
+                    <div className='mx-auto w-full lg:max-w-[400px]'>
+
                         <h2 className="text-center font-serifs text-[40px] lg:text-[60px] font-thin">
                             Đăng nhập
                         </h2>
 
                         <form onSubmit={handleSubmitLogin} className="mt-8 space-y-6">
                             <div>
-                                <input
-                                    type="email"
-                                    placeholder="Email"
-                                    className="input-primary block w-full"
-                                    value={email}
+                                <input type="email" placeholder='Email' 
+                                    className='input-primary block w-full' 
                                     onChange={(e) => setEmail(e.target.value)}
-                                    required
                                 />
                             </div>
                             <div>
-                                <input
-                                    type="password"
-                                    placeholder="Mật Khẩu"
-                                    className="input-primary block w-full"
-                                    value={password}
+                                <input type="password" placeholder='Mật Khẩu' 
+                                    className='input-primary block w-full' 
                                     onChange={(e) => setPassword(e.target.value)}
-                                    required
                                 />
                             </div>
-                            {error && <p className="text-red-500 text-center">{error}</p>}
-                            <button type="submit" className="block w-full button text-primaryGreen hover:text-ivory">
+                            <button type='submit' className='block w-full button text-primaryGreen hover:text-ivory'>
                                 Đăng Nhập
                             </button>
                         </form>
 
-                        <div className="font-mono mt-4 text-[14px] flex flex-col text-center gap-2">
-                            <p>
-                                <a href="" className="underline hover:no-underline hover:text-bgColor">
+                        <div className='font-mono mt-4 text-[14px] flex flex-col text-center gap-2'>
+                            <p className=''>
+                                <a href="" className='underline hover:no-underline hover:text-bgColor'>
                                     Quên mật khẩu?
                                 </a>
                             </p>
                             <p>
                                 Chưa có tài khoản? 
-                                <Link to="/signup" className="underline hover:no-underline hover:text-bgColor pl-2">
+                                <Link to="/signup" className='underline hover:no-underline hover:text-bgColor pl-2'>
                                     Đăng ký ngay!
                                 </Link>
                             </p>
                         </div>
+
                     </div>
                 </div>
             </article>
         </section>
-    );
+    )
 }
 
-export default LoginBody;
+export default LoginBody
